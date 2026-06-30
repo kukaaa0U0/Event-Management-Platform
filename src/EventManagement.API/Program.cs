b3eed95e8211ad27adbfa8eba2cbb774c9ffbceb
kukaaa0U0 <kirillishenko77@gmail.com>
2026-06-30 20:10:30 +0300

    Make API launch on a predictable local port

--- src/EventManagement.API/Program.cs
+++ src/EventManagement.API/Program.cs
@@ -8,20 +8,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseHttpsRedirection();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
+app.MapGet("/", () => Results.Redirect("/swagger"));
 app.MapControllers();
 
 app.Run();
